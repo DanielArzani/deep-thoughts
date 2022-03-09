6860cf2080d622743c92be1b1e910434001b830d
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-03-09 14:42:11 -0500

    render thoughts

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home';
+import Home from "./pages/Home";
 
+// establish the connection to the back-end server's /graphql endpoint
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+// instantiate a new cache object
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// Note how we wrap the entire returning JSX code with <ApolloProvider>. Because we're passing the client variable in as the value for the client prop in the provider, everything between the JSX tags will eventually have access to the server's API data through the client we set up
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React from "react";
+
+// useQuery is a hook
+import { useQuery } from "@apollo/client";
+
+import { QUERY_THOUGHTS } from "../utils/queries";
+import ThoughtList from "../components/ThoughtList";
 
 const Home = () => {
+  // use useQuery hook to make query request
+  // can also take in error alongside loading and data
+  // data -> an array of objects
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+
+  // the ? checks to make sure that data exists,it will return undefined if it doesn't
+  // making it equal to an empty array will prevent an error from being thrown, react will update it from an empty array into one with the data once its done loading
+  // we could probably use loading to deal with this
+  const thoughts = data?.thoughts || [];
+  // console.log(thoughts);
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )}
+        </div>
       </div>
     </main>
   );
