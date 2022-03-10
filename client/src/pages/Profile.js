02e6298f13bb976ca9be8bc4803e57f6bf316ee9
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-03-10 14:30:27 -0500

    add functionality for adding friends for authenticated user

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,23 +1,26 @@
 import React from "react";
-// Redirect, will allow us to redirect the user to another route within the application. Think of it like how we've used location.replace() in the past, but it leverages React Router's ability to not reload the browser!
 import { Redirect, useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
 
-import { QUERY_USER, QUERY_ME } from "../utils/queries";
-import Auth from "../utils/auth";
+// import ThoughtForm from "../components/ThoughtForm";
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
 
-const Profile = () => {
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { ADD_FRIEND } from "../utils/mutations";
+import Auth from "../utils/auth";
+
+const Profile = (props) => {
   const { username: userParam } = useParams();
 
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
 
   const user = data?.me || data?.user || {};
 
-  // redirect to personal profile page if username is the logged-in user's
+  // redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Redirect to="/profile" />;
   }
@@ -29,11 +32,21 @@ const Profile = () => {
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links
-        above to sign up or log in!
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
       </h4>
     );
   }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
@@ -41,6 +54,12 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -59,6 +78,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      {/* <div className="mb-3">{!userParam && <ThoughtForm />}</div> */}
     </div>
   );
 };
